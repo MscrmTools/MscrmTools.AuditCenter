aaea0f7625420bf30d03dbf6cbf1b5ffa84b9214
Tanguy TOUZARD <tanguy92@hotmail.com>
2016-09-23 08:44:38 +0200

    Initial release

---
+++ MsCrmTools.AuditCenter/MetadataHelper.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Metadata;
+using Microsoft.Xrm.Sdk.Query;
+using System.Text;
+using System.Xml;
+using Microsoft.Xrm.Sdk.Metadata.Query;
+
+namespace MsCrmTools.AuditCenter
+{
+    /// <summary>
+    /// Class for querying Crm Metadata
+    /// </summary>
+    internal class MetadataHelper
+    {
+        public static EntityMetadataCollection LoadEntities(IOrganizationService service)
+        {
+            var entityQueryExpression = new EntityQueryExpression
+            {
+                Properties = new MetadataPropertiesExpression("LogicalName", "DisplayName", "Attributes", "IsAuditEnabled", "ObjectTypeCode"),
+                AttributeQuery = new AttributeQueryExpression
+                {
+                    Properties = new MetadataPropertiesExpression("DisplayName", "LogicalName", "AttributeType", "IsAuditEnabled", "AttributeOf", "EntityLogicalName"),
+                }
+            };
+            var retrieveMetadataChangesRequest = new RetrieveMetadataChangesRequest
+            {
+                Query = entityQueryExpression,
+                ClientVersionStamp = null
+            };
+
+            return ((RetrieveMetadataChangesResponse)service.Execute(retrieveMetadataChangesRequest)).EntityMetadata;
+        }
+
+        /// <summary>
+        /// Retrieves main forms for the specified entity
+        /// </summary>
+        /// <param name="logicalName">Entity logical name</param>
+        /// <param name="oService">Crm organization service</param>
+        /// <returns>Document containing all forms definition</returns>
+        public static XmlDocument RetrieveEntityForms(string logicalName, IOrganizationService oService)
+        {
+            QueryByAttribute qba = new QueryByAttribute("systemform");
+            qba.Attributes.AddRange("objecttypecode", "type");
+            qba.Values.AddRange(logicalName, 2);
+            qba.ColumnSet = new ColumnSet(true);
+
+            EntityCollection ec = oService.RetrieveMultiple(qba);
+
+            StringBuilder allFormsXml = new StringBuilder();
+            allFormsXml.Append("<root>");
+
+            foreach (Entity form in ec.Entities)
+            {
+                allFormsXml.Append(form["formxml"]);
+            }
+
+            allFormsXml.Append("</root>");
+
+            XmlDocument docAllForms = new XmlDocument();
+            docAllForms.LoadXml(allFormsXml.ToString());
+
+            return docAllForms;
+        }
+    }
+}
---
+++ MsCrmTools.AuditCenter/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Audit Center")]
+[assembly: AssemblyDescription("Allows you to define audit settings")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MscrmTools")]
+[assembly: AssemblyProduct("MsCrmTools.AuditCenter")]
+[assembly: AssemblyCopyright("Copyright © MscrmTools 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("a8234074-9ff2-4a4c-b580-a2ad4507a116")]
+[assembly: AssemblyVersion("1.2016.9.1")]
+[assembly: AssemblyFileVersion("1.2016.9.1")]
